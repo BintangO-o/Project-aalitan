49b685177fd19fd7b241667e148865ce5442f479
M <setiamuhammad527@gmail.com>
2026-04-17 16:29:33 +0700

    nyambungin ke controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CRUDController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', [CRUDController::class, 'index']);
