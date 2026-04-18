13ebd16c14fec23cb7c36e47f911a12ca4cefb11
M <setiamuhammad527@gmail.com>
2026-04-18 19:41:57 +0700

    perbaikan controller

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -3,62 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class CRUDController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return view('index');
+        $todos = Todo::latest()->get();
+        return view('index', compact('todos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'task' => 'required|string|max:255',
+        ]);
+
+        Todo::create([
+            'task' => $request->task,
+        ]);
+
+        return redirect()->route('todos.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+        $todo->update([
+            'is_completed' => !$todo->is_completed
+        ]);
+
+        return redirect()->route('todos.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        Todo::findOrFail($id)->delete();
+        
+        return redirect()->route('todos.index');
     }
 }
